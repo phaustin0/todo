7d9d82535334d0f56c587841ba578c907a4af8b0
phaustin0 <zechy14@icloud.com>
2020-12-19 19:42:51 +0800

    created project

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+modules.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+}
